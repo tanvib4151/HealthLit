b5ac5cb87ea6d03767157d5e55d294e8f4680e8b
Tanvi Bharadwaj <tanvibharadwaj99@gmail.com>
2026-09-05 12:29:32 -0700

    Use professional CC0 figures for body map

--- components/body/BodyMap.tsx
+++ components/body/BodyMap.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-import Svg, { Ellipse, Path } from 'react-native-svg';
+import Svg, { Ellipse, Rect, SvgUri } from 'react-native-svg';
 
 import { getRegionLabel } from '../../utils/bodyRegions';
 import { useTheme } from '../../hooks/useTheme';
@@ -11,98 +11,110 @@ interface BodyMapProps {
 }
 
 type BodyView = 'front' | 'back';
-type Region = {
-  id: string;
-  priority?: number;
-  d?: string;
-  ellipse?: { cx: number; cy: number; rx: number; ry: number };
+type Zone =
+  | { id: string; priority?: number; kind: 'ellipse'; cx: number; cy: number; rx: number; ry: number }
+  | { id: string; priority?: number; kind: 'rect'; x: number; y: number; w: number; h: number; r?: number };
+
+type FigureAsset = {
+  uri: string;
+  width: number;
+  height: number;
+  zones: Zone[];
 };
 
-const VIEW_W = 260;
-const VIEW_H = 520;
-const SVG_H = 440;
+// CC0 figures by Sebastian Wallroth, Wikimedia Commons.
+// Keeping the artwork separate from the interaction geometry means the
+// figure can stay professionally proportioned while the hit areas remain
+// forgiving enough to use on a phone.
+const FRONT_URI =
+  'https://upload.wikimedia.org/wikipedia/commons/2/2c/Human_silhouette_gender_neutral_front.svg';
+const BACK_URI =
+  'https://upload.wikimedia.org/wikipedia/commons/8/85/Human_silhouette_gender_neutral_back.svg';
 
-const pathRegion = (id: string, d: string, priority = 8): Region => ({ id, d, priority });
-const ellipseRegion = (
-  id: string,
-  cx: number,
-  cy: number,
-  rx: number,
-  ry: number,
-  priority = 8,
-): Region => ({ id, ellipse: { cx, cy, rx, ry }, priority });
+const FRONT_ZONES: Zone[] = [
+  { id: 'head', kind: 'ellipse', cx: 280, cy: 86, rx: 70, ry: 82, priority: 5 },
+  { id: 'face', kind: 'ellipse', cx: 280, cy: 94, rx: 50, ry: 60, priority: 10 },
+  { id: 'neck', kind: 'rect', x: 244, y: 160, w: 72, h: 72, r: 22, priority: 10 },
 
-const FRONT_REGIONS: Region[] = [
-  ellipseRegion('head', 130, 49, 29, 36, 7),
-  ellipseRegion('face', 130, 51, 22, 28, 10),
-  pathRegion('neck', 'M116 82 C118 94 113 102 105 106 L155 106 C147 102 142 94 144 82 Z', 10),
-  pathRegion('shoulder_left', 'M103 105 C84 107 67 112 55 122 C49 129 45 139 43 151 C55 152 68 151 79 148 C84 130 91 116 103 108 Z', 9),
-  pathRegion('shoulder_right', 'M157 105 C176 107 193 112 205 122 C211 129 215 139 217 151 C205 152 192 151 181 148 C176 130 169 116 157 108 Z', 9),
-  pathRegion('chest_left', 'M92 113 C103 108 116 106 129 106 L129 169 C116 171 103 169 92 165 C87 151 84 135 84 121 Z', 7),
-  pathRegion('chest_right', 'M131 106 C144 106 157 108 168 113 L176 121 C176 135 173 151 168 165 C157 169 144 171 131 169 Z', 7),
-  pathRegion('upper_abdomen', 'M93 166 C105 170 117 172 130 172 C143 172 155 170 167 166 C164 185 164 199 167 211 C155 218 143 221 130 221 C117 221 105 218 93 211 C96 199 96 185 93 166 Z', 7),
-  pathRegion('lower_abdomen', 'M93 211 C105 218 117 221 130 221 C143 221 155 218 167 211 C169 228 167 242 161 255 C151 262 141 265 130 265 C119 265 109 262 99 255 C93 242 91 228 93 211 Z', 8),
-  pathRegion('hip_left', 'M99 253 C90 260 86 271 87 285 C94 295 106 301 121 301 L125 265 C114 264 106 260 99 253 Z', 9),
-  pathRegion('hip_right', 'M161 253 C170 260 174 271 173 285 C166 295 154 301 139 301 L135 265 C146 264 154 260 161 253 Z', 9),
-  pathRegion('upper_arm_left', 'M47 146 C41 165 39 185 39 202 C39 214 40 225 43 235 L59 232 C60 216 62 200 65 184 C68 169 73 156 79 147 Z'),
-  ellipseRegion('elbow_left', 45, 241, 12, 13, 10),
-  pathRegion('forearm_left', 'M41 253 C38 271 34 287 30 302 C27 313 30 322 36 324 C43 326 49 320 51 311 C55 291 59 271 59 253 Z'),
-  pathRegion('hand_left', 'M29 319 C21 326 18 337 22 347 C25 355 32 358 39 353 C45 349 48 339 46 326 Z', 10),
-  pathRegion('upper_arm_right', 'M213 146 C219 165 221 185 221 202 C221 214 220 225 217 235 L201 232 C200 216 198 200 195 184 C192 169 187 156 181 147 Z'),
-  ellipseRegion('elbow_right', 215, 241, 12, 13, 10),
-  pathRegion('forearm_right', 'M219 253 C222 271 226 287 230 302 C233 313 230 322 224 324 C217 326 211 320 209 311 C205 291 201 271 201 253 Z'),
-  pathRegion('hand_right', 'M231 319 C239 326 242 337 238 347 C235 355 228 358 221 353 C215 349 212 339 214 326 Z', 10),
-  pathRegion('thigh_left', 'M89 287 C84 308 84 330 87 352 C89 366 92 378 98 387 L121 382 C120 362 121 343 124 323 C126 311 126 302 123 297 C110 297 99 293 89 287 Z', 7),
-  ellipseRegion('knee_left', 105, 395, 15, 16, 10),
-  pathRegion('lower_leg_left', 'M94 411 C93 432 90 450 87 467 L112 467 C116 449 118 430 118 411 Z'),
-  pathRegion('ankle_left', 'M87 465 L112 465 L111 480 L87 480 Z', 10),
-  pathRegion('foot_left', 'M87 478 C79 486 73 492 75 497 C77 502 84 503 94 501 L113 498 L111 478 Z', 10),
-  pathRegion('thigh_right', 'M171 287 C176 308 176 330 173 352 C171 366 168 378 162 387 L139 382 C140 362 139 343 136 323 C134 311 134 302 137 297 C150 297 161 293 171 287 Z', 7),
-  ellipseRegion('knee_right', 155, 395, 15, 16, 10),
-  pathRegion('lower_leg_right', 'M166 411 C167 432 170 450 173 467 L148 467 C144 449 142 430 142 411 Z'),
-  pathRegion('ankle_right', 'M173 465 L148 465 L149 480 L173 480 Z', 10),
-  pathRegion('foot_right', 'M173 478 C181 486 187 492 185 497 C183 502 176 503 166 501 L147 498 L149 478 Z', 10),
+  { id: 'shoulder_left', kind: 'ellipse', cx: 188, cy: 248, rx: 84, ry: 58, priority: 9 },
+  { id: 'shoulder_right', kind: 'ellipse', cx: 371, cy: 248, rx: 84, ry: 58, priority: 9 },
+  { id: 'chest_left', kind: 'rect', x: 188, y: 255, w: 92, h: 164, r: 36, priority: 7 },
+  { id: 'chest_right', kind: 'rect', x: 279, y: 255, w: 92, h: 164, r: 36, priority: 7 },
+  { id: 'upper_abdomen', kind: 'rect', x: 214, y: 412, w: 131, h: 128, r: 34, priority: 7 },
+  { id: 'lower_abdomen', kind: 'rect', x: 208, y: 525, w: 143, h: 135, r: 34, priority: 8 },
+  { id: 'hip_left', kind: 'ellipse', cx: 229, cy: 650, rx: 70, ry: 58, priority: 9 },
+  { id: 'hip_right', kind: 'ellipse', cx: 330, cy: 650, rx: 70, ry: 58, priority: 9 },
+
+  { id: 'upper_arm_left', kind: 'rect', x: 92, y: 278, w: 92, h: 248, r: 42, priority: 8 },
+  { id: 'elbow_left', kind: 'ellipse', cx: 116, cy: 520, rx: 48, ry: 48, priority: 10 },
+  { id: 'forearm_left', kind: 'rect', x: 63, y: 530, w: 86, h: 230, r: 40, priority: 8 },
+  { id: 'hand_left', kind: 'ellipse', cx: 91, cy: 782, rx: 52, ry: 68, priority: 10 },
+
+  { id: 'upper_arm_right', kind: 'rect', x: 375, y: 278, w: 92, h: 248, r: 42, priority: 8 },
+  { id: 'elbow_right', kind: 'ellipse', cx: 443, cy: 520, rx: 48, ry: 48, priority: 10 },
+  { id: 'forearm_right', kind: 'rect', x: 410, y: 530, w: 86, h: 230, r: 40, priority: 8 },
+  { id: 'hand_right', kind: 'ellipse', cx: 468, cy: 782, rx: 52, ry: 68, priority: 10 },
+
+  { id: 'thigh_left', kind: 'rect', x: 185, y: 665, w: 98, h: 268, r: 44, priority: 7 },
+  { id: 'knee_left', kind: 'ellipse', cx: 232, cy: 935, rx: 49, ry: 47, priority: 10 },
+  { id: 'lower_leg_left', kind: 'rect', x: 190, y: 965, w: 83, h: 163, r: 38, priority: 8 },
+  { id: 'ankle_left', kind: 'ellipse', cx: 230, cy: 1119, rx: 39, ry: 34, priority: 10 },
+  { id: 'foot_left', kind: 'ellipse', cx: 220, cy: 1165, rx: 65, ry: 37, priority: 10 },
+
+  { id: 'thigh_right', kind: 'rect', x: 276, y: 665, w: 98, h: 268, r: 44, priority: 7 },
+  { id: 'knee_right', kind: 'ellipse', cx: 327, cy: 935, rx: 49, ry: 47, priority: 10 },
+  { id: 'lower_leg_right', kind: 'rect', x: 286, y: 965, w: 83, h: 163, r: 38, priority: 8 },
+  { id: 'ankle_right', kind: 'ellipse', cx: 329, cy: 1119, rx: 39, ry: 34, priority: 10 },
+  { id: 'foot_right', kind: 'ellipse', cx: 339, cy: 1165, rx: 65, ry: 37, priority: 10 },
 ];
 
-const BACK_REGIONS: Region[] = [
-  ellipseRegion('head', 130, 49, 29, 36, 8),
-  pathRegion('neck', 'M116 82 C118 94 113 102 105 106 L155 106 C147 102 142 94 144 82 Z', 10),
-  pathRegion('shoulder_left', 'M101 105 C82 108 65 114 53 125 C47 132 43 142 42 154 C55 155 68 153 80 149 C85 130 91 116 101 109 Z', 9),
-  pathRegion('shoulder_right', 'M159 105 C178 108 195 114 207 125 C213 132 217 142 218 154 C205 155 192 153 180 149 C175 130 169 116 159 109 Z', 9),
-  pathRegion('shoulder_blade_left', 'M91 113 C102 108 114 106 129 106 L129 174 C116 174 104 170 94 164 C89 148 87 130 91 113 Z', 8),
-  pathRegion('shoulder_blade_right', 'M131 106 C146 106 158 108 169 113 C173 130 171 148 166 164 C156 170 144 174 131 174 Z', 8),
-  pathRegion('mid_back', 'M94 165 C105 171 117 174 130 174 C143 174 155 171 166 165 C164 184 164 202 168 216 C156 224 143 228 130 228 C117 228 104 224 92 216 C96 202 96 184 94 165 Z', 7),
-  pathRegion('lower_back_left', 'M92 216 C103 224 115 228 129 228 L126 267 C113 267 103 263 95 256 C91 242 90 229 92 216 Z', 8),
-  pathRegion('lower_back_right', 'M131 228 C145 228 157 224 168 216 C170 229 169 242 165 256 C157 263 147 267 134 267 Z', 8),
-  pathRegion('glute_left', 'M95 255 C87 263 84 276 87 290 C95 301 108 307 127 307 L126 267 C113 267 103 263 95 255 Z', 9),
-  pathRegion('glute_right', 'M165 255 C173 263 176 276 173 290 C165 301 152 307 133 307 L134 267 C147 267 157 263 165 255 Z', 9),
-  pathRegion('upper_arm_left', 'M47 148 C41 167 39 186 39 203 C39 216 40 227 43 237 L59 234 C60 218 62 202 65 186 C68 171 73 158 80 149 Z'),
-  ellipseRegion('elbow_left', 45, 243, 12, 13, 10),
-  pathRegion('forearm_left', 'M41 255 C38 273 34 289 30 304 C27 315 30 324 36 326 C43 328 49 322 51 313 C55 293 59 273 59 255 Z'),
-  pathRegion('hand_left', 'M29 321 C21 328 18 339 22 349 C25 357 32 360 39 355 C45 351 48 341 46 328 Z', 10),
-  pathRegion('upper_arm_right', 'M213 148 C219 167 221 186 221 203 C221 216 220 227 217 237 L201 234 C200 218 198 202 195 186 C192 171 187 158 180 149 Z'),
-  ellipseRegion('elbow_right', 215, 243, 12, 13, 10),
-  pathRegion('forearm_right', 'M219 255 C222 273 226 289 230 304 C233 315 230 324 224 326 C217 328 211 322 209 313 C205 293 201 273 201 255 Z'),
-  pathRegion('hand_right', 'M231 321 C239 328 242 339 238 349 C235 357 228 360 221 355 C215 351 212 341 214 328 Z', 10),
-  pathRegion('hamstring_left', 'M88 292 C84 312 85 333 88 352 C90 366 94 378 99 387 L122 382 C120 361 121 342 124 323 C126 315 127 308 127 304 C111 305 98 301 88 292 Z', 8),
-  pathRegion('hamstring_right', 'M172 292 C176 312 175 333 172 352 C170 366 166 378 161 387 L138 382 C140 361 139 342 136 323 C134 315 133 308 133 304 C149 305 162 301 172 292 Z', 8),
-  ellipseRegion('knee_left', 105, 395, 15, 16, 10),
-  ellipseRegion('knee_right', 155, 395, 15, 16, 10),
-  pathRegion('calf_left', 'M94 411 C92 429 90 447 88 466 L112 466 C116 448 118 430 118 411 Z', 8),
-  pathRegion('calf_right', 'M166 411 C168 429 170 447 172 466 L148 466 C144 448 142 430 142 411 Z', 8),
-  pathRegion('heel_left', 'M87 465 L112 465 L111 480 C106 491 99 497 90 498 C83 496 80 489 82 479 Z', 10),
-  pathRegion('heel_right', 'M173 465 L148 465 L149 480 C154 491 161 497 170 498 C177 496 180 489 178 479 Z', 10),
+const BACK_ZONES: Zone[] = [
+  { id: 'head', kind: 'ellipse', cx: 280, cy: 84, rx: 70, ry: 82, priority: 8 },
+  { id: 'neck', kind: 'rect', x: 244, y: 158, w: 72, h: 72, r: 22, priority: 10 },
+  { id: 'shoulder_left', kind: 'ellipse', cx: 188, cy: 246, rx: 84, ry: 58, priority: 9 },
+  { id: 'shoulder_right', kind: 'ellipse', cx: 371, cy: 246, rx: 84, ry: 58, priority: 9 },
+  { id: 'shoulder_blade_left', kind: 'ellipse', cx: 229, cy: 320, rx: 67, ry: 88, priority: 8 },
+  { id: 'shoulder_blade_right', kind: 'ellipse', cx: 330, cy: 320, rx: 67, ry: 88, priority: 8 },
+  { id: 'mid_back', kind: 'rect', x: 211, y: 365, w: 137, h: 175, r: 36, priority: 7 },
+  { id: 'lower_back_left', kind: 'rect', x: 205, y: 520, w: 75, h: 125, r: 30, priority: 8 },
+  { id: 'lower_back_right', kind: 'rect', x: 279, y: 520, w: 75, h: 125, r: 30, priority: 8 },
+  { id: 'glute_left', kind: 'ellipse', cx: 229, cy: 648, rx: 70, ry: 66, priority: 9 },
+  { id: 'glute_right', kind: 'ellipse', cx: 330, cy: 648, rx: 70, ry: 66, priority: 9 },
+
+  { id: 'upper_arm_left', kind: 'rect', x: 92, y: 276, w: 92, h: 246, r: 42, priority: 8 },
+  { id: 'elbow_left', kind: 'ellipse', cx: 116, cy: 516, rx: 48, ry: 48, priority: 10 },
+  { id: 'forearm_left', kind: 'rect', x: 63, y: 526, w: 86, h: 226, r: 40, priority: 8 },
+  { id: 'hand_left', kind: 'ellipse', cx: 91, cy: 772, rx: 52, ry: 68, priority: 10 },
+
+  { id: 'upper_arm_right', kind: 'rect', x: 375, y: 276, w: 92, h: 246, r: 42, priority: 8 },
+  { id: 'elbow_right', kind: 'ellipse', cx: 443, cy: 516, rx: 48, ry: 48, priority: 10 },
+  { id: 'forearm_right', kind: 'rect', x: 410, y: 526, w: 86, h: 226, r: 40, priority: 8 },
+  { id: 'hand_right', kind: 'ellipse', cx: 468, cy: 772, rx: 52, ry: 68, priority: 10 },
+
+  { id: 'hamstring_left', kind: 'rect', x: 185, y: 674, w: 98, h: 250, r: 44, priority: 8 },
+  { id: 'hamstring_right', kind: 'rect', x: 276, y: 674, w: 98, h: 250, r: 44, priority: 8 },
+  { id: 'knee_left', kind: 'ellipse', cx: 232, cy: 925, rx: 49, ry: 47, priority: 10 },
+  { id: 'knee_right', kind: 'ellipse', cx: 327, cy: 925, rx: 49, ry: 47, priority: 10 },
+  { id: 'calf_left', kind: 'rect', x: 190, y: 955, w: 83, h: 158, r: 38, priority: 8 },
+  { id: 'calf_right', kind: 'rect', x: 286, y: 955, w: 83, h: 158, r: 38, priority: 8 },
+  { id: 'heel_left', kind: 'ellipse', cx: 229, cy: 1135, rx: 54, ry: 46, priority: 10 },
+  { id: 'heel_right', kind: 'ellipse', cx: 330, cy: 1135, rx: 54, ry: 46, priority: 10 },
 ];
+
+const ASSETS: Record<BodyView, FigureAsset> = {
+  front: { uri: FRONT_URI, width: 559, height: 1204, zones: FRONT_ZONES },
+  back: { uri: BACK_URI, width: 559, height: 1190, zones: BACK_ZONES },
+};
 
 export function BodyMap({ selected, onToggle }: BodyMapProps) {
   const theme = useTheme();
   const [view, setView] = useState<BodyView>('front');
-  const regions = view === 'front' ? FRONT_REGIONS : BACK_REGIONS;
+  const asset = ASSETS[view];
   const selectedLabels = selected.map(getRegionLabel).join(', ');
 
-  const orderedRegions = useMemo(
-    () => [...regions].sort((a, b) => (a.priority ?? 1) - (b.priority ?? 1)),
-    [regions],
+  const orderedZones = useMemo(
+    () => [...asset.zones].sort((a, b) => (a.priority ?? 1) - (b.priority ?? 1)),
+    [asset.zones],
   );
 
   const styles = useMemo(
@@ -132,7 +144,14 @@ export function BodyMap({ selected, onToggle }: BodyMapProps) {
           color: theme.colors.inkSecondary,
         },
         toggleTextSelected: { color: theme.colors.onPrimary },
-        diagramWrap: { minHeight: SVG_H },
+        figureFrame: {
+          width: 232,
+          alignSelf: 'center',
+          position: 'relative',
+          backgroundColor: theme.colors.surface,
+          borderRadius: theme.radius.xl,
+          overflow: 'hidden',
+        },
         hint: { ...theme.typography.caption, textAlign: 'center' },
         selectedText: {
           ...theme.typography.bodySecondary,
@@ -171,38 +190,55 @@ export function BodyMap({ selected, onToggle }: BodyMapProps) {
         })}
       </View>
 
-      <View style={styles.diagramWrap}>
-        <Svg width="100%" height={SVG_H} viewBox={`0 0 ${VIEW_W} ${VIEW_H}`}>
-          {orderedRegions.map((region) => {
-            const isSelected = selected.includes(region.id);
+      <View style={[styles.figureFrame, { aspectRatio: asset.width / asset.height }]}>
+        <SvgUri width="100%" height="100%" uri={asset.uri} />
+        <Svg
+          width="100%"
+          height="100%"
+          viewBox={`0 0 ${asset.width} ${asset.height}`}
+          style={StyleSheet.absoluteFillObject}
+        >
+          {orderedZones.map((zone) => {
+            const isSelected = selected.includes(zone.id);
             const common = {
-              onPress: () => onToggle(region.id),
-              fill: isSelected ? theme.colors.primary : theme.colors.surfaceMuted,
-              stroke: isSelected ? theme.colors.primaryPressed : theme.colors.border,
-              strokeWidth: isSelected ? 2.2 : 1.15,
-              opacity: isSelected ? 0.95 : 1,
+              onPress: () => onToggle(zone.id),
+              fill: isSelected ? theme.colors.primary : 'rgba(0,0,0,0.001)',
+              fillOpacity: isSelected ? 0.3 : 1,
+              stroke: isSelected ? theme.colors.primary : 'transparent',
+              strokeWidth: isSelected ? 4 : 0,
             };
 
-            if (region.ellipse) {
+            if (zone.kind === 'ellipse') {
               return (
                 <Ellipse
-                  key={region.id}
-                  cx={region.ellipse.cx}
-                  cy={region.ellipse.cy}
-                  rx={region.ellipse.rx}
-                  ry={region.ellipse.ry}
+                  key={zone.id}
+                  cx={zone.cx}
+                  cy={zone.cy}
+                  rx={zone.rx}
+                  ry={zone.ry}
                   {...common}
                 />
               );
             }
 
-            return region.d ? <Path key={region.id} d={region.d} {...common} /> : null;
+            return (
+              <Rect
+                key={zone.id}
+                x={zone.x}
+                y={zone.y}
+                width={zone.w}
+                height={zone.h}
+                rx={zone.r ?? 0}
+                ry={zone.r ?? 0}
+                {...common}
+              />
+            );
           })}
         </Svg>
       </View>
 
       <Text style={styles.hint}>
-        {view === 'front' ? 'Front regions' : 'Back regions'} — tap every area that applies.
+        {view === 'front' ? 'Front view' : 'Back view'} — tap directly on the figure.
       </Text>
 
       {selected.length > 0 ? (
